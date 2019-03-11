7955f088d48a20f13c62c179f49c04e2b9823a57
Vireak <44746913+Vireak-Sok@users.noreply.github.com>
2019-03-11 16:14:04 +0700

    Create PalindromeNumber.c

---
+++ Basic C Programs/PalindromeNumber.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main() {
+	int inp,mol,res=0,pal;
+	scanf("%d",&inp);
+	pal=inp;
+	while (inp!=0) {
+		mol=inp%10;
+		inp/=10;
+		res=res*10+mol;
+	}
+	if(res==pal) {
+		printf("palindrome");
+	}
+	else {
+		printf("Not palindrome");
+	}
+}
